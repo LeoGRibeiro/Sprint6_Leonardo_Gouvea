8f8a02d68f9d6ef342887d8c5d5dd48cc659a978
LeoGRibeiro <101428785+LeoGRibeiro@users.noreply.github.com>
2022-07-12 19:25:39 -0300

    Finalização funções

--- consumir_library_teste.robot
+++ consumir_library_teste.robot
@@ -3,10 +3,25 @@ Documentation           Arquivo de testes de arquivos python
 Library                 library_teste.py
 
 * Test Cases *
-Teste Library 1
-    ${json}             Get Endpoint Usuarios     ${1}
-    Log To Console      ${json}
+Retornar Usuários com Carrinho
+    [tags]              usuarios_com_carrinho      
+    ${users}            Usuarios Com Carrinho      ${5}  # Inserir a quantidade de usuários deseja retornar
+    Log To Console      Lista de usuários com carrinho: ${users}
 
-Teste Library 2
-    ${users}             Pegar Quantidade usuarios
-    Log To Console      ${users}
+Receita Esperada Do Estoque
+    [tags]              receita_esperada
+    ${receita}          Receita Esperada
+    Log To Console      Receita Esperada: R$${receita}
+
+Reposicao de Estoque
+    [tags]              reposicao_estoque
+    ${produtos}         Reposicao Estoque         ${2000}  # Inserir quantidade, retornará produtos com quantidade abaixo da inserida, e a quantidade para chegar na desejada 
+    Log To Console      Produtos que precisam de reposição: ${produtos}                  
+
+Fazer Reposicao de Estoque
+    [tags]              fazer_reposicao_estoque
+    Fazer Reposicao Estoque     ${1504}
+
+Cadastrar Produtos
+    [tags]              cadastrar_produtos 
+    Cadastrar Produtos          ${2}    # Inserir a quantidade de produtos que deseja criar
--- library_teste.py
+++ library_teste.py
@@ -1,28 +1,106 @@
+from lib2to3.pgen2 import token
 import requests 
 import json
+import random
+from faker import Faker
 
-def Get_Endpoint_Usuarios():
-    lista = []  
-    r = requests.get("http://localhost:3000/usuarios")
-    para_json = r.json()          # Transformar a resposta em json manipulavel
+
+#_________________Função que cadastra produtos____________________________________________________# 
+def Cadastrar_Produtos(vezes):
+    fake = Faker()
+    login = {"email": "fulano123@qa.com", "password": "teste123"}
+    i = requests.post("http://localhost:3000/login", data=login)
+    response = i.json()
+    token_auth = response["authorization"]
+
+    headers = {"monitor": 'false', "Authorization": token_auth}
     
+    for c in range(vezes):
+        nome = fake.name()
+        preco = random.randint(20, 1500)
+        descricao = fake.text()
+        quantidade = random.randint(2000, 5000)
+        produto = {"nome": nome, "preco": preco, "descricao": descricao, "quantidade": quantidade}
+        i = requests.post(f"http://localhost:3000/produtos", headers=headers, data=produto)
 
-    return para_json
 
-
-
-
-
-def Pegar_Quantidade_Usuarios():
-    r = requests.get("http://localhost:3000/usuarios")
-    request_json = r.json()
-
-    return request_json["quantidade"]
+#_________________Função que retorna usuários com carrinho_______________________________________#
+def Usuarios_Com_Carrinho(quantidade):
+    lista_users_carrinho = []
+    lista_aleatoria_users = []   
+    headers = {"monitor": 'false'}
     
+    r = requests.get("http://localhost:3000/carrinhos")
+    carrinhos = r.json()                       
+    
+    for c in range(carrinhos["quantidade"]):
+        idUsuario = carrinhos["carrinhos"][c]["idUsuario"]
+        lista_users_carrinho.append(idUsuario)
 
+    while len(lista_aleatoria_users) < quantidade:
+        pesquisa_id = random.choice(lista_users_carrinho)
+        u = requests.get(f"http://localhost:3000/usuarios/{pesquisa_id}", headers=headers)
+        dados_user = u.json()
+        if dados_user not in lista_aleatoria_users:
+            lista_aleatoria_users.append(dados_user)
+    
+    return lista_aleatoria_users
+        
+#_________________Função que retorna receita a partir de todo o estoque________________________#
+def Receita_Esperada():
+    receita_total = 0
+    t = requests.get("http://localhost:3000/produtos")
+    produtos = t.json()
 
-x = Get_Endpoint_Usuarios()
-print(x)
+    for c in range(produtos["quantidade"]):
+        preco = produtos["produtos"][c]["preco"]
+        quant_prod = produtos["produtos"][c]["quantidade"]
+        receita_prod = preco * quant_prod
+        receita_total += receita_prod
 
-y = Pegar_Quantidade_Usuarios()
-print(y)
+    return receita_total
+
+#_________________Função que mostra quais produtos estão abaixo da quantidade desejada_______#
+def Reposicao_Estoque(quantidade):
+    lista_prods_reposicao = []
+    t = requests.get("http://localhost:3000/produtos")
+    produtos = t.json()
+
+    for c in range(produtos["quantidade"]):
+        nome = produtos["produtos"][c]["nome"]
+        preco = produtos["produtos"][c]["preco"]
+        descricao = produtos["produtos"][c]["descricao"]
+        quant_prod = produtos["produtos"][c]["quantidade"]
+        idProduto = produtos["produtos"][c]["_id"]
+
+        if quant_prod < quantidade:
+            quant_compra = quantidade - quant_prod
+            produto = {"nome": nome, "idProduto": idProduto, "quantidade_para_compra": quant_compra}
+            lista_prods_reposicao.append(produto)
+            
+    return lista_prods_reposicao
+   
+ #_________________Função que faz a "compra" de produtos para chegar na quantidade desejada___#  
+def Fazer_Reposicao_Estoque(quantidade):
+    login = {"email": "fulano123@qa.com", "password": "teste123"}
+    i = requests.post("http://localhost:3000/login", data=login)
+    response = i.json()
+    token_auth = response["authorization"]
+
+    headers = {"monitor": 'false', "Authorization": token_auth}
+    lista_prods_reposicao = []
+    t = requests.get("http://localhost:3000/produtos")
+    produtos = t.json()
+
+    for c in range(produtos["quantidade"]):
+        nome = produtos["produtos"][c]["nome"]
+        preco = produtos["produtos"][c]["preco"]
+        descricao = produtos["produtos"][c]["descricao"]
+        quant_prod = produtos["produtos"][c]["quantidade"]
+        idProduto = produtos["produtos"][c]["_id"]
+
+        if quant_prod < quantidade:
+            quant_compra = quantidade - quant_prod
+            produto = {"nome": nome, "preco": preco, "descricao": descricao, "quantidade": quantidade}
+
+            i = requests.put(f"http://localhost:3000/produtos/{idProduto}", headers=headers, data=produto)
